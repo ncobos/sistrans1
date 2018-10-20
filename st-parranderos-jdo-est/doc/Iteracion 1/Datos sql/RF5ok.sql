398be412966d5f8e23da1cd1bab391152e0b0be4
Nicolás <n.cobos@uniandes.edu.co>
2018-10-20 17:43:29 -0500

    iteracion2

--- st-parranderos-jdo-est/doc/Iteracion 1/Datos sql/RF5ok.sql
+++ st-parranderos-jdo-est/doc/Iteracion 1/Datos sql/RF5ok.sql
@@ -1,15 +1,15 @@
-INSERT INTO A_BODEGA
-(id, capacidadvolumen, capacidadpeso, producto, sucursal, existencias, capacidadproductos)
-VALUES (1,2000,7500,1,2,48,150);
+INSERT INTO A_ALMACENAMIENTO
+(id, capacidadvolumen, capacidadpeso, producto, sucursal, nivelabastecimientobodega, existencias, capacidadproductos, tipo)
+VALUES (1,2000,7500,1,2,0,0,200,'Bodega');
 
-INSERT INTO A_BODEGA
-(id, capacidadvolumen, capacidadpeso, producto, sucursal, existencias, capacidadproductos)
-VALUES (2,2000,6500,3,2,75,100);
+INSERT INTO A_ALMACENAMIENTO
+(id, capacidadvolumen, capacidadpeso, producto, sucursal, nivelabastecimientobodega, existencias, capacidadproductos, tipo)
+VALUES (2,2000,6500,3,2,0,0,300,'Bodega');
 
-INSERT INTO A_BODEGA
-(id, capacidadvolumen, capacidadpeso, producto, sucursal, existencias, capacidadproductos)
-VALUES (3,2000,7000,5,3,25,55);
+INSERT INTO A_ALMACENAMIENTO
+(id, capacidadvolumen, capacidadpeso, producto, sucursal, nivelabastecimientobodega, existencias, capacidadproductos, tipo)
+VALUES (3,2000,7000,5,3,0,0,240,'Bodega');
 
-INSERT INTO A_BODEGA
-(id, capacidadvolumen, capacidadpeso, producto, sucursal, existencias, capacidadproductos)
-VALUES (4,2500,7000,8,3,200,384);
+INSERT INTO A_ALMACENAMIENTO
+(id, capacidadvolumen, capacidadpeso, producto, sucursal, nivelabastecimientobodega, existencias, capacidadproductos, tipo)
+VALUES (4,2500,7000,8,3,0,0,210,'Bodega');
--- st-parranderos-jdo-est/doc/Iteracion 1/Datos sql/RF6ok.sql
+++ st-parranderos-jdo-est/doc/Iteracion 1/Datos sql/RF6ok.sql
@@ -1,15 +1,15 @@
-INSERT INTO A_ESTANTE
-(id, capacidadvolumen, capacidadpeso, producto, sucursal, nivelabastecimientobodega, existencias, capacidadproductos)
-VALUES (1,200,120,5,2,10,52,78);
+INSERT INTO A_ALMACENAMIENTO
+(id, capacidadvolumen, capacidadpeso, producto, sucursal, nivelabastecimientobodega, existencias, capacidadproductos, tipo)
+VALUES (1,200,120,5,2,10,52,80,'Estante');
 
-INSERT INTO A_ESTANTE
-(id, capacidadvolumen, capacidadpeso, producto, sucursal, nivelabastecimientobodega, existencias, capacidadproductos)
-VALUES (2, 220,120,2,2,10,57,320);
+INSERT INTO A_ALMACENAMIENTO
+(id, capacidadvolumen, capacidadpeso, producto, sucursal, nivelabastecimientobodega, existencias, capacidadproductos, tipo)
+VALUES (2, 220,120,2,2,10,57,81,'Estante');
 
-INSERT INTO A_ESTANTE
-(id, capacidadvolumen, capacidadpeso, producto, sucursal, nivelabastecimientobodega, existencias, capacidadproductos)
-VALUES (3, 210,120,3,3,10,55,120);
+INSERT INTO A_ALMACENAMIENTO
+(id, capacidadvolumen, capacidadpeso, producto, sucursal, nivelabastecimientobodega, existencias, capacidadproductos, tipo)
+VALUES (3, 210,120,3,3,10,55,70,'Estante');
 
-INSERT INTO A_ESTANTE
-(id, capacidadvolumen, capacidadpeso, producto, sucursal, nivelabastecimientobodega, existencias, capacidadproductos)
-VALUES (4, 190,120,7,3,10,60,156);
+INSERT INTO A_ALMACENAMIENTO
+(id, capacidadvolumen, capacidadpeso, producto, sucursal, nivelabastecimientobodega, existencias, capacidadproductos, tipo)
+VALUES (4, 190,120,7,3,10,60,73,'Estante');
